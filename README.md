61a81da02b88bf665d07bfef660342a3f46584e6
Travis Rich <travis.s.rich@gmail.com>
2023-03-30 16:14:45 -0400

    Init commit with functioning script

---
+++ README.md
@@ -0,0 +1,14 @@
+# Underlay export to Wiki Table
+
+This repo offers an exporting script that demonstrates updating a wikitable from an underlay collection.
+
+## Running the script
+To get started, begin by running 
+```
+cp .env.sample .env
+npm install
+```
+
+- Update the .env file with your wiki login credentials.
+- Verify the `ulCollection`, `ulCollectionVersion`, and `wikiPageDestination` values at the top of `main.js`
+- Run `npm start`
---
+++ main.js
@@ -0,0 +1,80 @@
+import Wikiapi from 'wikiapi';
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+/* Underlay & Wikipedia parameters */
+/* ------------------------------- */
+const ulCollection = 'reliable-sources/vaccine-topics-xmsdk95byp';
+const ulCollectionVersion = '0.0.2';
+const wikiPageDestination = 'Wikipedia:Vaccine safety/Sources/Assessments';
+/* ------------------------------- */
+
+/* Load the Underlay collection data and parse it into Wikitable formatting */
+const parseJson = async () => {
+	/* Set UL collection parameters */
+	const ulCollectionUrl = `https://www.underlay.org/api/${ulCollection}/download?version=${ulCollectionVersion}`;
+
+	/* Fetch UL collection data */
+	const response = await fetch(dataUrl, { json: true });
+	const body = await response.text();
+	const data = JSON.parse(body);
+
+	/* Iterate over each Source and format it into Wikitable formatting */
+	const sources = data.data.Source;
+	const tableText = sources.reduce((prev, curr) => {
+		return `${prev}
+|-
+|${curr.type}
+|${curr.name}
+|${curr.assessment}		
+|${curr.discussionForum}
+|${curr.discussionLastDate}
+|
+|[${curr.url}]
+|${curr.uses}
+		`.trim();
+	}, '');
+	return tableText;
+};
+
+const run = async () => {
+	/* Login to wikiAPI */
+	const wiki = new Wikiapi();
+	const login_options = {
+		user_name: process.env.username,
+		password: process.env.password,
+	};
+	await wiki.login(login_options);
+
+	/* Set page that the table will be written to. */
+	await wiki.page(wikiPageDestination);
+
+	/* Load values in Wikitable text format from Underlay collection */
+	const dataValues = await parseJson();
+
+	/* Call wiki.edit function to write to the destination Wiki page */
+	/* Note, this will overwrite the entire content of the page, if  */
+	/* you want to append or edit, manipulate the `pageData` value.  */
+	await wiki.edit(
+		(_pageData) => {
+			return `{| class="wikitable sortable"
+|-
+! rowspan=2 | Type
+! rowspan=2 width=30% | Source name
+! rowspan=2 width=2%  | Assess<br/>ment
+!   colspan=3   | Discussions
+! rowspan=2 | URL
+! rowspan=2 | Uses
+|-
+!  Forums
+!  width=2%  | Date (last)
+!  width=30% | Discussion summary
+${dataValues}
+|}`;
+		},
+		{ bot: 1 }
+	);
+};
+
+run();
